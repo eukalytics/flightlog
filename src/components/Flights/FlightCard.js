e1734c09a850942eba0342936fc0cf945a1af383
eukalytics <eukalytics@protonmail.com>
2021-11-24 17:50:31 +0000

    add flight detail styles

--- src/components/Flights/FlightCard.js
+++ src/components/Flights/FlightCard.js
@@ -3,10 +3,10 @@ export default function FlightCard({ pilot, craft, date, notes }) {
     return (
         <div className="bg-gray-700 m-4 p-4 grid grid-cols-10 rounded">
             <FlightDate date={date} />
-            <div className="col-span-8">
-                <h3>{pilot} ({craft})</h3>
-                <h2 className>gFlights</h2>
-                <p>{notes}</p>
+            <div className="col-span-8 text-gray-300 px-4">
+                <p className="pb-2"><span className="font-semibold">Pilot:</span> {pilot}</p>
+                <p className="pb-2"><span className="font-semibold">Spacecraft:</span> {craft}</p>
+                <p className=""><span className="font-semibold">Notes:</span> {notes}</p>
             </div>
         </div>
     )

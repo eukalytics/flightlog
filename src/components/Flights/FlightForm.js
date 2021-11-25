c34fcdfdab77b23ea5d184e5d95b8237f08f9e90
eukalytics <eukalytics@protonmail.com>
2021-11-25 11:38:16 +0000

    add new flight form

---
+++ src/components/Flights/FlightForm.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+export default function FlightForm() {
+    const [newFlight, setNewFlight] = useState({
+        title: '',
+        craft: '',
+        date: '',
+        notes: '',
+    });
+    const handleTitleChange = (e) => {
+        setNewFlight((newFlight) => {
+            return { 
+                ...newFlight,
+                title: e.target.value,
+            }
+        });
+    }
+    const handleCraftChange = (e) => {
+        setNewFlight((newFlight) => {
+            return { 
+                ...newFlight,
+                craft: e.target.value,
+            }
+        });
+    }
+    const handleDateChange = (e) => {
+        setNewFlight((newFlight) => {
+            return { 
+                ...newFlight,
+                date: e.target.value,
+            }
+        });
+    }
+    const handleNotesChange = (e) => {
+        setNewFlight((newFlight) => {
+            return { 
+                ...newFlight,
+                notes: e.target.value,
+            }
+        });
+    }
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const submittedFlight = {
+            ...newFlight, 
+            date: new Date(newFlight.date)
+        };
+        console.log(submittedFlight);
+        setNewFlight({
+            title: '',
+            craft: '',
+            date: '',
+            notes: '',
+        });
+    }   
+    return(
+        <form 
+            onSubmit={handleFormSubmit}
+            className="bg-gray-700 m-4 p-4 rounded"
+        >
+            <div>
+                <label >Pilot</label>
+                <input type="text" name="pilot" value={newFlight.title} onChange={handleTitleChange} />
+            </div>
+            <div>
+                <label>Craft</label>
+                <input type="text" name="craft" value={newFlight.craft} onChange={handleCraftChange}/>
+            </div>
+            <div>
+                <label> Date</label>
+                <input type="date" name="date" value={newFlight.date} min="2018-01-01" max="2022-12-31" onChange={handleDateChange} />
+            </div>
+            <div>
+                <label>Notes</label>
+                <textarea name="notes" value={newFlight.notes} onChange={handleNotesChange} />
+            </div>
+            <div>
+                <button type="submit">Add Flight</button>
+            </div>
+        </form>
+    )
+}
--- src/components/Flights/FlightList.js
+++ src/components/Flights/FlightList.js
@@ -1,9 +1,11 @@
 import FlightCard from "./FlightCard";
+import FlightForm from "./FlightForm";
 
 export default function FlightList({flights}) {
     return (
         <div className="bg-gray-900 p-5 rounded">
             <h2 className="text-gray-200 text-3xl font-semibold text-center">Flight Log</h2>
+            <FlightForm />
             <FlightCard
               pilot={flights[0].pilot}
               craft={flights[0].craft}

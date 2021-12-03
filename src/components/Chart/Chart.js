75f803553f0fbfba453f168df127aaa4777be65f
eukalytics <eukalytics@protonmail.com>
2021-12-03 13:02:38 +0000

    update flight chart labels

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,7 +2,7 @@ import ChartBar from "./ChartBar";
 
 export default function Chart({datapoints, maxFlights}) {
     return (
-        <div className="bg-gray-500 m-5 px-10 grid grid-cols-12 h-60 border-2 border-white pt-10">
+        <div className="bg-gray-700 m-5 px-10 grid grid-cols-12 h-60 border-2 border-white pt-10">
             {datapoints.map((point) => (
                 <ChartBar point={point} maxFlights={maxFlights} key={point.month} />
             ))}
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -5,11 +5,13 @@ export default function ChartBar({ point, maxFlights }) {
     if (maxFlights > 0){
         barFillHeight = (100 - (Math.round((point.flights / maxFlights) * 100))) + '%';
     }
+
+    let labelColour = point.flights ? 'text-gray-700' : 'text-gray-400';
     return (
         <div>
             <div className="h-full mx-5 relative bg-yellow-400">
-                <div className='bg-gray-500' style={{height: barFillHeight}}></div>
-                <div className='text-center absolute inset-x-0 bottom-0 text-gray-900'>{point.month}</div>
+                <div className='bg-gray-700' style={{height: barFillHeight}}></div>
+                <div className={`text-center absolute inset-x-0 bottom-0 ${labelColour}`}>{point.month}</div>
             </div>
         </div>
     )
